749f11305d862941e3cfddc5461e8095f0be2c6f
rishikawavse <147980527+rishikawavse@users.noreply.github.com>
2024-05-18 14:22:08 +0000

    conversion of datatype

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,35 @@
+ let score= true
+// console.log(typeof score)
+
+ let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+/*
+"33" => 33
+"33abc" => NaN
+true/false => 1/0
+null => 0
+undefined => NaN
+*/
+
+let isLoggedIn= " "
+
+let booleanIsloogedIn= Boolean(isLoggedIn)
+//console.log(booleanIsloogedIn)
+
+/*
+1 => true
+0 =>false
+"" => false
+but if we give space in it ,return true i.e., "  "=> true
+"xyz" => true
+null => false
+undefined => false
+*/
+
+let a= true
+ let stringtrue=String(a)
+ console.log(stringtrue)
+ console.log(typeof stringtrue) // string
+ console.log(typeof a) // boolean
