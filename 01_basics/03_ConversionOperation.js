8fe0a403b22e35ea538962b2ada6a3be7f5461d6
rishikawavse <147980527+rishikawavse@users.noreply.github.com>
2024-05-24 01:08:29 +0000

    coperations

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -1,35 +1,57 @@
- let score= true
-// console.log(typeof score)
+//  let score= true
+// // console.log(typeof score)
 
- let valueInNumber=Number(score)
-// console.log(typeof valueInNumber)
-// console.log(valueInNumber)
+//  let valueInNumber=Number(score)
+// // console.log(typeof valueInNumber)
+// // console.log(valueInNumber)
 
-/*
-"33" => 33
-"33abc" => NaN
-true/false => 1/0
-null => 0
-undefined => NaN
-*/
+// /*
+// "33" => 33
+// "33abc" => NaN
+// true/false => 1/0
+// null => 0
+// undefined => NaN
+// */
 
-let isLoggedIn= " "
+// let isLoggedIn= " "
 
-let booleanIsloogedIn= Boolean(isLoggedIn)
-//console.log(booleanIsloogedIn)
+// let booleanIsloogedIn= Boolean(isLoggedIn)
+// //console.log(booleanIsloogedIn)
 
-/*
-1 => true
-0 =>false
-"" => false
-but if we give space in it ,return true i.e., "  "=> true
-"xyz" => true
-null => false
-undefined => false
-*/
+// /*
+// 1 => true
+// 0 =>false
+// "" => false
+// but if we give space in it ,return true i.e., "  "=> true
+// "xyz" => true
+// null => false
+// undefined => false
+// */
 
-let a= true
- let stringtrue=String(a)
- console.log(stringtrue)
- console.log(typeof stringtrue) // string
- console.log(typeof a) // boolean
+// let a= true
+//  let stringtrue=String(a)
+//  console.log(stringtrue)
+//  console.log(typeof stringtrue) // string
+//  console.log(typeof a) // boolean
+
+ /* *******operations******* */
+ console.log(2+2);
+ console.log(2-2);
+ console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+let str1 ="Hello"
+let str2 =" Rishika"
+let str3=str1+str2
+console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+ "2")
+
+console.log(+true);
+console.log(+"");
+
