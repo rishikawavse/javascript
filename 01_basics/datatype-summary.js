f37708f37ae3d37631935ec1887ba2add7b27b9c
rishikawavse <147980527+rishikawavse@users.noreply.github.com>
2024-05-27 16:23:27 +0000

    about types of data

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,21 @@
+//primitive datatype
+
+// 7 types: String,Number, Boolean,null,undefined ,Symbol,BigInt
+
+// Reference(Non primitive)
+// array,Objects,functions
+const bigNumber=333345678333344545n
+console.log(typeof bigNumber)
+
+const myFunction =function(){
+    console.log("hello world")
+}
+
+ 
+
+let myobj={
+    nname:"Rishika",
+    age:19,
+}
+
+console.log(typeof myobj)
